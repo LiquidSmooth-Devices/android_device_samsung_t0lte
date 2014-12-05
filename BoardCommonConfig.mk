446d13b6d404d08ffd0fb5af7206e2c87f1c8413
sbrissen <sbrissen@hotmail.com>
2014-12-05 14:23:09 -0500

    t0lte: initial L bring up
    -selinux policies still need work
    -switch fstab to by-name
    
    Change-Id: I397931d0aa2ea7887774007acf40c9f03e66fb14

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -53,6 +53,8 @@ BOARD_SEPOLICY_DIRS += \
     device/samsung/t0lte/selinux
 
 BOARD_SEPOLICY_UNION += \
+    bluetooth.te \
+    radio.te \
     file_contexts \
     te_macros \
     device.te \
@@ -63,10 +65,18 @@ BOARD_SEPOLICY_UNION += \
     kickstart.te \
     mediaserver.te \
     netmgrd.te \
-    qmux.te \
+    qmiproxy.te \
+    qmuxd.te \
     rild.te \
     secril.te \
+    servicemanager.te \
+    sysinit.te \
     system.te \
+    system_server.te \
+    time_daemon.te \
     ueventd.te \
-    wpa_supplicant.te
+    vold.te \
+    wpa.te \
+    wpa_supplicant.te \
+    zygote.te
 
--- common.mk
+++ common.mk
@@ -75,8 +75,7 @@ $(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=SamsungQualcommRIL \
-    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0
+    mobiledata.interfaces=pdp0,gprs,ppp0,rmnet0,rmnet1
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
